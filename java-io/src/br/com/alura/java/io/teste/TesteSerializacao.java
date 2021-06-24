6d1f6c666233ec3b565856c32f1a2cc6d3cadc32
RaphaelSan <raphaelsan.dev@outlook.com>
2021-06-24 17:38:37 -0300

    subindo io

---
+++ java-io/src/br/com/alura/java/io/teste/TesteSerializacao.java
@@ -0,0 +1,24 @@
+package br.com.alura.java.io.teste;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+public class TesteSerializacao {
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		
+		//String nome = "Raphael Santos";
+		
+//		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("objeto.bin"));	
+//		oos.writeObject(nome);																	//transformar um objeto em fluxo binário é chamado Serialização.
+//		oos.close();
+		
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("objeto.bin"));
+		String nome = (String) ois.readObject();												//transformar um fluxo binário em objeto é chamado desserialização
+		System.out.println(nome);
+		
+	}
+	
+}
